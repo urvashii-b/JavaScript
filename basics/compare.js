83a4f5bc76767156bf995a769c807decc6547745
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-05-01 01:08:26 +0530

    comparison of datatypes

---
+++ basics/compare.js
@@ -0,0 +1,18 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+// typescript doesn't let you compare two different datatypes together
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null<0);
+console.log(null!=0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+
+// === checks value AND datatype
