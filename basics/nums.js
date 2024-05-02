fdbb3c82c8ac3fdec79e8c10a01b2979710db042
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-05-02 23:26:32 +0530

    math

--- basics/nums.js
+++ basics/nums.js
@@ -11,3 +11,18 @@ console.log(no.toPrecision(2)); // 1.2e+2
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++ MATH +++++++++++++++++++++
+
+console.log(Math.abs(-2))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+// DICE 
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max-min+1))+ min)
